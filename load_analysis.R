012b51962abeab716fa522aaf24efae0c7849d19
Robert Willhoft <willhoft_robert@roberts.edu>
2019-08-02 17:43:43 -0400

    Code to load results of analysis

---
+++ load_analysis.R
@@ -0,0 +1,9 @@
+# Load the results of the
+# Peer-graded Assignment: 
+# Getting and Cleaning Data Course Project
+# RG Willhoft, 2019
+
+load( file = "analysis_data.Rda")
+load( file = "analysis_data_means.Rda")
+
+# Additional code as needed
